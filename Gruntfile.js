85120b9c353efc9f62aedcefb33c05f7d391f3d8
Samuel Whiteley <bling.nuke@gmail.com>
2017-02-03 21:27:10 -0700

    The bad thing worked, so now I'm making it part of the css build process :b

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,47 @@
 'use strict';
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.initConfig({
-    cssmin: {
-      options: {
-        shorthandCompacting: false,
-        roundingPrecision: -1
-      },
-      target: {
-        files: {
-          'static/css/style.css': ['static/css/style.css']
-        }
-      }
-    },
-    watch: {
-      files: ["scss/*.less", "scss/*/*.less"],
-      tasks: ["build:css"]
-    },
-    less: {
-      development: {
-        options: {
-          paths: ["scss"],
+    grunt.initConfig({
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'static/css/style.css': ['static/css/style.css']
+                }
+            }
         },
-        files: {
-          "static/css/style.css": "scss/style.less"
-        }
-      },
-    },
-  });
+        watch: {
+            files: ["scss/*.less", "scss/*/*.less"],
+            tasks: ["build:css"]
+        },
+        less: {
+            development: {
+                options: {
+                    paths: ["scss"],
+                },
+                files: {
+                    "static/css/style.css": "scss/style.less"
+                }
+            },
+        },
+        copy: {
+            main: {
+                src: 'static/css/style.css',
+                dest: 'layouts/partials/style.css',
+            }
+        },
+    });
 
-  grunt.registerTask('build:css', ['less', 'cssmin']);
+    grunt.registerTask('build:css', ['less', 'cssmin', 'copy']);
 
-  grunt.registerTask('default', ['build:css']);
+    grunt.registerTask('default', ['build:css']);
 };
